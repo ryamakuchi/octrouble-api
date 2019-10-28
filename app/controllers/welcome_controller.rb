79e499fcd5d7a00e87f53a7bab15c504ca37bd3b
rry <r.yamakuchi@gmail.com>
2019-10-29 04:33:27 +0900

    URL 検索ロジックを追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,10 @@ class WelcomeController < ApplicationController
   def index
     if params[:url].present?
       @url = params[:url]
-      sample = Sample.new(url: params[:url])
+      sample = Sample.new(url: @url)
 
       @result = sample.issues
+      @message = sample.message if sample.message.present?
     end
   end
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,11 +1,34 @@
 class Sample
   include ActiveModel::Model
 
+  URL_REGEXP = /github\.com\/[\w-]+\/[\w-]+/
+  REPOSITORY_REGEXP = /[\w-]+\/[\w-]+/
+
   attr_accessor :url
+  attr_reader :message
 
   validates :url, presence: true
 
   def issues
-    csv = `octrouble issues`
+    repo = url_check
+    return message = '正しいリポジトリを指定してください' if repo.nil?
+    puts "repo: #{repo}"
+    `bundle exec octrouble issues #{repo}`
+  end
+
+  private
+
+  def url_check
+    if url =~ URL_REGEXP
+      repo = url.slice(URL_REGEXP).slice(11..-1)
+      repo.chop if repo[-1] == '/'
+
+    elsif url =~ REPOSITORY_REGEXP
+      repo = url.slice(REPOSITORY_REGEXP)
+
+    else
+      repo = nil
+    end
+    repo
   end
 end
