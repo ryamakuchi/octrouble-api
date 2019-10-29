674f79a5763f6499ed01f5b698b2ea9ba801553a
rry <r.yamakuchi@gmail.com>
2019-10-29 21:46:03 +0900

    add spec: RSpec の設定とテストケースを書いた

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-rails'
   gem 'pry-byebug'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'rspec_junit_formatter'
 end
 
 group :development do
@@ -44,9 +47,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'rspec-rails'
-  gem 'factory_bot_rails'
-  gem 'rspec_junit_formatter'
 end
 
 group :test do
--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,5 +1,26 @@
-require 'rails_helper'
+describe Sample, type: :model do
+  describe '#issues' do
+    before do
+    end
 
-RSpec.describe Sample, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context 'GitHub URL で入力された場合' do
+      it '該当リポジトリの Issues を返す'
+    end
+
+    context 'user/repo で入力された場合' do
+      it '該当リポジトリの Issues を返す'
+    end
+
+    context 'user/repo や GitHub URL 以外の文字列が入力された場合' do
+      it '正しいリポジトリを指定してください'
+    end
+
+    context '該当の Issues が 0件だった場合' do
+      it 'Issues は 0件です'
+    end
+
+    context '該当の Issues が 0件だった場合' do
+      it 'リポジトリが見つかりません'
+    end
+  end
 end
